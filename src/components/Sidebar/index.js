59ffa9c635a24ea948d366aed4f5a3bcdb7d4eca
Rohit kumar <rohitprj.1282@gmail.com>
2025-03-16 19:07:16 +0530

    Add initial project files including styles, components, and configuration

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,83 @@
+import "./index.scss";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import {
+  faEnvelope,
+  faHome,
+  faScrewdriverWrench,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link, NavLink } from "react-router-dom";
+
+import codechefLogo from "../../assets/images/codechef.png";
+import LogoS from "../../assets/images/logo-s.png";
+import LogoSubtitle from "../../assets/images/output-onlinepngtools.png";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="nav-bar">
+        <Link className="logo" to="/">
+          {/* <img src={LogoS} alt="Logo" /> */}
+          <img className="sub-logo" src={LogoSubtitle} alt="sudip" />
+          {/* <h3>Rohit</h3> */}
+        </Link>
+        <nav>
+          <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+          <NavLink activeclassname="active" className="about-link" to="/about">
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+
+          <NavLink
+            activeclassname="active"
+            className="skills-link"
+            to="/skills"
+          >
+            <FontAwesomeIcon icon={faScrewdriverWrench} color="#4d4d4e" />
+          </NavLink>
+
+          <NavLink
+            activeclassname="active"
+            className="contact-link"
+            to="/contact"
+          >
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+        </nav>
+        <ul>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/rohit-kumar-4b5800218/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#b9b9b9" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/Rohitprj"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} color="#b9b9b9" />
+            </a>
+          </li>
+          {/* <li>
+            <a
+              href="https://www.codechef.com/users/metal_oopa"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={codechefLogo} width="24" alt="Codechef" />
+            </a>
+          </li> */}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
